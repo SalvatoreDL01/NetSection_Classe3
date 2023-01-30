df0e7c2b7070e70d136ee2e15955312399a74e3f
giuseppe <granogiuseppe01@gmail.com>
2023-01-30 12:42:40 +0100

    Creazione classe risposta e creazione classe CommnetoDAO vuota

---
+++ src/main/java/ServiziEStorage/CommentoDAO.java
@@ -0,0 +1,24 @@
+package ServiziEStorage;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CommentoDAO {
+
+    /*create table Commento(
+dataScrittura datetime,
+creatore int,
+sezione int not null,
+discussione varchar(30) not null,
+contenuto varchar(300) not null,
+punteggio numeric(5) default 0,
+foreign key(creatore) references UtenteRegistrato(id) on delete cascade,
+foreign key(sezione,discussione) references Discussione(sezione,titolo) on delete cascade on update cascade,
+primary key(dataScrittura, creatore)
+);*/
+
+
+
+}
