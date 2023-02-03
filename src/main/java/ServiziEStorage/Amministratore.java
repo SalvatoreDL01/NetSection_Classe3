37c153c4c422ea0b5fc95c1b6c5f446f84fa198d
SalvatoreDL01 <104557779+SalvatoreDL01@users.noreply.github.com>
2023-02-03 17:04:31 +0100

    Aggiunta commenti alle classi Amministratore, AmministratoreDAO, Commento e CommentoDAO. Aggiunta dei seguenti metodi alla classe CommentoDAO: -doRetriveByID; -doRetriveByDiscussione; -doRetriveByCreatore; -doRetriveBySegnalati; -doSave; -doRemove. Va aggiunto uno o più metodi update, per i quali si consiglia la revisione dei requisiti.

--- src/main/java/ServiziEStorage/Amministratore.java
+++ src/main/java/ServiziEStorage/Amministratore.java
@@ -1,45 +1,45 @@
 package ServiziEStorage;
-
+/*Oggetto che rappresenta un Amministratore. Contiene il parametro int idAdmin, String username, email, e pass*/
 public class Amministratore {
 
     private int idAdmin;
     private String username, email, pass;
-
+/*Costruttore di un oggetto Amministratore. Necessita int idAdmin, String username, String pass, String email*/
     public Amministratore(int idAdmin, String username, String pass, String email) {
         this.idAdmin = idAdmin;
         this.username = username;
         this.pass = pass;
         this.email = email;
     }
-
+    /*Metodo che ritorna un oggetto int idAdmin*/
     public int getIdAdmin() {
         return idAdmin;
     }
-
+    /*Metodo che setta un oggetto int idAdmin*/
     public void setIdAdmin(int idAdmin) {
         this.idAdmin = idAdmin;
     }
-
+    /*Metodo che ritorna un oggetto String username*/
     public String getUsername() {
         return username;
     }
-
+    /*Metodo che setta un oggetto String username*/
     public void setUsername(String username) {
         this.username = username;
     }
-
+    /*Metodo che ritorna un oggetto String email*/
     public String getEmail() {
         return email;
     }
-
+    /*Metodo che setta un oggetto String email*/
     public void setEmail(String email) {
         this.email = email;
     }
-
+    /*Metodo che ritorna un oggetto String pass*/
     public String getPass() {
         return pass;
     }
-
+    /*Metodo che setta un oggetto String pass*/
     public void setPass(String pass) {
         this.pass = pass;
     }
