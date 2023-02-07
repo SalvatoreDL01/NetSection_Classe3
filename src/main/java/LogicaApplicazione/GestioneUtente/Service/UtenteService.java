f60fcd01772083fc4fc9889b7988b409e0e3a83b
SalvatoreDL01 <104557779+SalvatoreDL01@users.noreply.github.com>
2023-02-07 11:26:19 +0100

    Correzione DAO in UtenteService

--- src/main/java/LogicaApplicazione/GestioneUtente/Service/UtenteService.java
+++ src/main/java/LogicaApplicazione/GestioneUtente/Service/UtenteService.java
@@ -11,25 +11,28 @@ import ServiziEStorage.Entry.UtenteRegistrato;
 import java.util.ArrayList;
 
 public class UtenteService {
+    public static final UtenteRegistratoDAO utenteRegistratoDAO = new UtenteRegistratoDAO();
+    public static final UtenteNetflixDAO utenteNetflixDAO = new UtenteNetflixDAO();
+    public static final EliminazioneDAO eliminazioneDAO = new EliminazioneDAO();
     static public void banUtente(int idUserToBan){
-        ArrayList<UtenteRegistrato> listU= UtenteRegistratoDAO.retriveAll();
+        ArrayList<UtenteRegistrato> listU= utenteRegistratoDAO.retriveAll();
 
         for (UtenteRegistrato u: listU) {
             if(u.getId()==idUserToBan){
                 Eliminazione e=new Eliminazione(u.getId(), u.getUsername(), u.getEmail());
-                EliminazioneDAO.doSave(e);
-                UtenteRegistratoDAO.remove(idUserToBan);
+                eliminazioneDAO.doSave(e);
+                utenteRegistratoDAO.remove(idUserToBan);
                 System.out.println("L'utente è stato rimosso correttamente");
             }
         }
     }
 
     static public void kickUtente(int idUserToKick, Discussione discussione){
-        ArrayList<UtenteRegistrato> listU=UtenteRegistratoDAO.retriveAll();
+        ArrayList<UtenteRegistrato> listU=utenteRegistratoDAO.retriveAll();
 
         for (UtenteRegistrato u: listU) {
             if(u.getId()==idUserToKick){
-                UtenteRegistratoDAO.removeUtente(discussione, u);
+                utenteRegistratoDAO.removeUtente(discussione, u);
                 System.out.println("L'utente è stato kickato dalla conversazione");
             }
         }
@@ -37,7 +40,7 @@ public class UtenteService {
 
     static public UtenteRegistrato checkUtente(String user, String password){
 
-        ArrayList<UtenteRegistrato> list = UtenteRegistratoDAO.retriveAll();
+        ArrayList<UtenteRegistrato> list = utenteRegistratoDAO.retriveAll();
         for(UtenteRegistrato u: list){
             if(user.equals(u.getEmail()) && password.equals(u.getPass()))
                 return u;
@@ -47,7 +50,7 @@ public class UtenteService {
 
     static public UtenteNetflix checkUtenteNetflix(String user, String password){
 
-        ArrayList<UtenteNetflix> list = (ArrayList<UtenteNetflix>) UtenteNetflixDAO.retiveAll();
+        ArrayList<UtenteNetflix> list = (ArrayList<UtenteNetflix>) utenteNetflixDAO.retiveAll();
         for(UtenteNetflix u: list){
             if(user.equals(u.getEmail()) && password.equals(u.getPass()))
                 return u;
