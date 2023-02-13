1b6206e706fc1eeeedafd62849b9ba7c6ae64bb1
chris <christian.civetta14@gmail.com>
2023-02-13 15:46:03 +0100

    aggiunte funzioni all'admin quali ban utente e modifica della propria mail

--- src/main/java/LogicaApplicazione/Admin/Service/AdminServiceImp.java
+++ src/main/java/LogicaApplicazione/Admin/Service/AdminServiceImp.java
@@ -15,6 +15,18 @@ public class AdminServiceImp implements AdminService{
     UtenteRegistratoDAO utenteRegistratoDAO= new UtenteRegistratoDAO();
     EliminazioneDAO eliminazioneDAO= new EliminazioneDAO();
 
+    public void banUtente(int idUserToBan){
+        ArrayList<UtenteRegistrato> listU= utenteRegistratoDAO.retriveAll();
+
+        for (UtenteRegistrato u: listU) {
+            if(u.getId()==idUserToBan){
+                Eliminazione e=new Eliminazione(u.getId(), u.getUsername(), u.getEmail());
+                eliminazioneDAO.doSave(e);
+                utenteRegistratoDAO.remove(idUserToBan);
+                System.out.println("L'utente è stato rimosso correttamente");
+            }
+        }
+    }
     public void editMail(String nuovaMail, int idAdmin){
         Amministratore a= ammDao.doRetriveById(idAdmin);
 
