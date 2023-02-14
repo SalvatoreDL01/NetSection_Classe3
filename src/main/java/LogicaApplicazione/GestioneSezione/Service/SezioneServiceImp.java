ff4c315f2de03a3ae056239508cebdc6a81ca5b0
SalvatoreDL01 <104557779+SalvatoreDL01@users.noreply.github.com>
2023-02-14 12:03:18 +0100

    immagini fix

--- src/main/java/LogicaApplicazione/GestioneSezione/Service/SezioneServiceImp.java
+++ src/main/java/LogicaApplicazione/GestioneSezione/Service/SezioneServiceImp.java
@@ -58,7 +58,7 @@ public class SezioneServiceImp implements SezioneService{
         }
 
         try {
-            String dirPath = "C:/Users/utente/IdeaProjects/NetSection_Classe3/src/main/webapp/css/icone/Immagini/"+s.getIdSezione();
+            String dirPath = "C:/Users/utente/IdeaProjects/NetSection_Classe3/src/main/webapp/css/icone/Immagini/";
             File f = new File(dirPath);
             f.setWritable(true);
             System.out.println(f.canWrite());
@@ -69,8 +69,8 @@ public class SezioneServiceImp implements SezioneService{
             String fileName = part.getSubmittedFileName();
             if(fileName.equals(""))
                 return false;
-            String immagine = "css/icone/Immagini/" +s.getIdSezione()+"/"+ fileName;
-            String path = dirPath + "/" + fileName;
+            String immagine = "css/icone/Immagini/"+ fileName;
+            String path = dirPath + fileName;
 
             s.setImmagine(immagine);
 
