6558d979968b11c2f047341b495c716232dd4a8c
giuseppe <granogiuseppe01@gmail.com>
2023-02-14 21:24:39 +0100

    Aggiunta logica per votare i commenti

---
+++ src/main/java/LogicaApplicazione/GestionDiscussione/Controller/VotoCommentoController.java
@@ -0,0 +1,33 @@
+package LogicaApplicazione.GestionDiscussione.Controller;
+
+import LogicaApplicazione.GestionDiscussione.Service.DiscussioneServiceImp;
+import ServiziEStorage.DAO.CommentoDAO;
+import ServiziEStorage.Entry.Commento;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "VotoCommentoController", value = "/VotoCommentoController")
+public class VotoCommentoController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String data = request.getParameter("data");
+        int creatore = Integer.parseInt(request.getParameter("creatore"));
+        DiscussioneServiceImp discussioneServiceImp = new DiscussioneServiceImp();
+        Commento c = discussioneServiceImp.ottieniCommento(data, creatore);
+        c.setPunteggio(c.getPunteggio()+1);
+        new CommentoDAO().update(c);
+        String path = "DiscussiController?tipo=iscritto&sezione=" + request.getParameter("idSezione") + "&titolo=" + request.getParameter("discussione");
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(path);
+        requestDispatcher.forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
